93c765865f74d0db02f8f6e32672fd263d113fa1
dantr <huynhngdantr@gmail.com>
2023-12-07 18:17:05 +0700

    state manager for the modal open or close using zustand

---
+++ app/hooks/userRegisterModal.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+//type
+interface RegisterModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+//set init value and action to modify value
+const useRegisterModal = create<RegisterModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useRegisterModal;
